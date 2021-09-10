e171ce18a980fb36401ba848841789611de03059
0AYU0 <awy001@ucsd.edu>
2021-09-09 23:53:50 -0700

    basic-dash-features

--- pseudo_data.py
+++ pseudo_data.py
@@ -12,17 +12,17 @@ runningSum = 0
 # create pandas dataframe from csv file
 df = pandas.read_csv('TotalPopulationByCountry - Data.csv')
 for index, row in df.iterrows():
-  # filter numeric codes for countries
-  if(row['ISO 3166-1 numeric code'] < 900): 
-    # represent population as ranges of corresponding to each country
-    country_running_total[row['Location'].strip()] = [ 
-        runningSum, runningSum + int(row['2021'].replace(" ", ""))]
-    runningSum += int(row['2021'].replace(" ", ""))
+    # filter numeric codes for countries
+    if(row['ISO 3166-1 numeric code'] < 900):
+        # represent population as ranges of corresponding to each country
+        country_running_total[row['Location'].strip()] = [
+            runningSum, runningSum + int(row['2021'].replace(" ", ""))]
+        runningSum += int(row['2021'].replace(" ", ""))
 
 
 def random_point_in_country(country_name):
     # reading shapefile with pyshp library
-    shapes = shapefile.Reader('World_Countries.shp')
+    shapes = shapefile.Reader('country_border_data/World_Countries.shp')
     # getting feature(s) that match the country name
     country = [s for s in shapes.records() if country_name in s]
     if len(country) == 0:
@@ -47,34 +47,41 @@ def random_point_in_country(country_name):
             return p.x, p.y
 
 
+def generate_user():
+    rand = random.randrange(runningSum)
+    for key, value in country_running_total.items():
+        # if random value in population range specified
+        if rand > value[0] and rand <= value[1]:
+            # grab location within country for the user
+            location = random_point_in_country(key)
+            # normal distribution for availability to represent load balancing
+            rand_availability = numpy.random.normal(0.5, 0.2)
+            # proportion of caregivers and patients to depict
+            #caregiver_or_patient = 'Caregiver' if random.random() > 0.7 else 'Patient'
+            if location != -1 and rand_availability > 0:
+                # define and write simulated data in a dictionary
+                user_data = {
+                    'index': i,
+                    'country': key,
+                    'longitude': location[0],
+                    'latitude': location[1],
+                    'availability': rand_availability,
+                    # 'user_type': caregiver_or_patient,
+                }
+                return user_data
+
+
 f = open('clinician_data.csv', 'w')
 
 with f:
-  fnames = ['index', 'country', 'longitude', 'latitude', 'availability']
-  # sets fields of CSV and ensure each line ends with new line
-  caregiver_writer = csv.DictWriter(f, fieldnames=fnames, lineterminator='\n')
-  caregiver_writer.writeheader()
+    fnames = ['index', 'country', 'longitude', 'latitude', 'availability']
+    # sets fields of CSV and ensure each line ends with new line
+    caregiver_writer = csv.DictWriter(
+        f, fieldnames=fnames, lineterminator='\n')
+    caregiver_writer.writeheader()
 
-  for i in range(100):
-    rand = random.randrange(runningSum)
-    for key, value in country_running_total.items():
-      # if random value in population range specified  
-      if rand > value[0] and rand <= value[1]: 
-        # grab location within country for the user
-        location = random_point_in_country(key)
-        # normal distribution for availability to represent load balancing
-        rand_availability = numpy.random.normal(0.5, 0.2)
-        # proportion of caregivers and patients to depict
-        #caregiver_or_patient = 'Caregiver' if random.random() > 0.7 else 'Patient'
-        if location != -1 and rand_availability > 0:
-          # define and write simulated data in a dictionary
-          user_data = {
-            'index': i, 
-            'country': key, 
-            'longitude': location[0], 
-            'latitude': location[1], 
-            'availability': rand_availability, 
-            #'user_type': caregiver_or_patient,
-            }
+    for i in range(100):
+      user_data = generate_user()
+      if user_data:
         caregiver_writer.writerow(user_data)
 f.close()
--- visualize_data.py
+++ visualize_data.py
@@ -3,6 +3,8 @@ import pandas as pd
 import dash
 from dash import dcc
 from dash import html
+from dash.dependencies import Input, Output
+from pseudo_data import *
 
 # read CSVs into dataframes
 caregiver_df = pd.read_csv('clinician_data.csv')
@@ -16,50 +18,66 @@ fig = go.Figure()
 
 # add caregiver user data
 fig.add_trace(go.Scattergeo(
-  locationmode='USA-states',
-  lon=caregiver_df['longitude'],
-  lat=caregiver_df['latitude'],
-  text=caregiver_df['text'],
-  marker=dict(
-      size=caregiver_df['availability'] * scale,
-      cmax=1,
-      cmin=0,
-      color=caregiver_df['availability'],
-      line_width=0,
-      sizemode='area',
-      colorbar=dict(
-          title="Caregiver Availability"
-      ),
-      colorscale="mint"
-  )))
+    locationmode='USA-states',
+    lon=caregiver_df['longitude'],
+    lat=caregiver_df['latitude'],
+    text=caregiver_df['text'],
+    marker=dict(
+        size=caregiver_df['availability'] * scale,
+        cmax=1,
+        cmin=0,
+        color=caregiver_df['availability'],
+        line_width=0,
+        sizemode='area',
+        colorbar=dict(
+            title="Caregiver Availability"
+        ),
+        colorscale="mint"
+    )))
 
 # add caregiver distances data
 for i in range(len(caregiver_distances_df)):
-  fig.add_trace(
-    go.Scattergeo(
-        locationmode = 'USA-states',
-        lon = [caregiver_distances_df['patient_lon'][i], caregiver_distances_df['caregiver_lon'][i]],
-        lat = [caregiver_distances_df['patient_lat'][i], caregiver_distances_df['caregiver_lat'][i]],
-        mode = 'lines',
-        line = dict(width = 2,color = 'red'),
-        opacity = float(caregiver_distances_df['distance'].min()) / float(caregiver_distances_df['distance'][i]),
-    )
-  )
+    if float(caregiver_distances_df['distance'].min()) / float(caregiver_distances_df['distance'][i]) > 0.25:
+        fig.add_trace(
+            go.Scattergeo(
+                locationmode='USA-states',
+                lon=[caregiver_distances_df['patient_lon'][i],
+                     caregiver_distances_df['caregiver_lon'][i]],
+                lat=[caregiver_distances_df['patient_lat'][i],
+                     caregiver_distances_df['caregiver_lat'][i]],
+                mode='lines',
+                line=dict(width=2, color='red'),
+                opacity=float(caregiver_distances_df['distance'].min(
+                )) / float(caregiver_distances_df['distance'][i]),
+            )
+        )
 
 fig.update_layout(
-    title_text = 'MedECC Caregiver Matchmaking Visualization<br>(Hover for Caregiver Country and Location)',
-    showlegend = False,
-    geo = dict(
-      showland = True,
-      landcolor = 'rgb(243, 243, 243)',
-      countrycolor = 'rgb(204, 204, 204)',
+    title_text='MedECC Caregiver Matchmaking Visualization<br>(Hover for Caregiver Country and Location)',
+    showlegend=False,
+    geo=dict(
+        showland=True,
+        landcolor='rgb(243, 243, 243)',
+        countrycolor='rgb(204, 204, 204)',
     ),
 )
 fig.update_geos(lataxis_showgrid=True, lonaxis_showgrid=True)
 
 app = dash.Dash()
 app.layout = html.Div([
-    dcc.Graph(figure=fig)
+    dcc.Graph(figure=fig),
+    html.Button('Generate New Patient', id='gen-patient', n_clicks=0),
+    html.Div(id='num-patients-added',
+             children='')
 ])
+
+@app.callback(Output('num-patients-added', 'children'),
+              Input('gen-patient', 'n_clicks'))
+def add_patient(n_clicks):
+    user_data = generate_user()
+    return '{} new patients have been added'.format(
+        n_clicks
+    )
+
 
 app.run_server(debug=True, use_reloader=False)
